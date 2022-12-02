9ab2ce4d6ff2ab01984effe201f613979714767f
Aamir-Khurshid <108178409+Aamir-Khurshid@users.noreply.github.com>
2022-12-03 00:33:02 +0530

    Add files via upload

---
+++ one.java
@@ -0,0 +1,11 @@
+package nothing;
+
+import java.util.Scanner;
+
+public class one {
+    public static void main(String[] args) {
+        Scanner next = new Scanner(System.in);
+        System.out.println(next.next());// used for just one word
+        System.out.println(next.nextLine());// used for printing complete statement
+    }
+}
